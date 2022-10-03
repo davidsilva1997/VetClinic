2c72d3f56c4189e8185454381c06fcc90146eb94
David Gonçalves da Silva <david_goncalvessilva@hotmail.com>
2022-10-03 12:54:55 +0100

    Creation of web app

---
+++ VetClinicWeb/Controllers/VetController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace VetClinicWeb.Controllers
+{
+    public class VetController : Controller
+    {
+        static readonly HttpClient httpClient = new HttpClient();
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync("https://localhost:7012/Vet");
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadFromJsonAsync<IEnumerable<Models.Vet>>();
+
+                return View(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetVet([FromRoute] Guid id)
+        {
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(string.Format("{0}/{1}", "https://localhost:7012/Vet", id));
+                response.EnsureSuccessStatusCode();
+                var responseBody = await response.Content.ReadFromJsonAsync<Models.Vet>();
+
+                return View(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ VetClinicWeb/Models/Vet.cs
@@ -0,0 +1,10 @@
+﻿namespace VetClinicWeb.Models
+{
+    public class Vet
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
